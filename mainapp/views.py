c039a903c4a254992fddec1d04ced5c9d37c5f64
Omid Adib <akbaradibfar4@gmail.com>
2020-04-04 21:41:20 +0430

    improve landing view and add dynamic data to html page

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render, redirect
 
+from .utils import *
+from sinpegroup import settings
+
 
 def landing(request):
-    return render(request, 'mainapp/Index.html')
+    services = get_services()  # 6
+    portfolios = get_portfolios()  # 3
+    land_desc = get_landingpage_desc()  # 1
+    MEDIA_URL = settings.MEDIA_URL
+    context = {
+        'services': services,
+        'portfolios': portfolios,
+        'land_desc': land_desc,
+        'MEDIA_URL': MEDIA_URL,
+    }
+    return render(request, 'mainapp/Index.html', context)
 
 
 def index(request):
