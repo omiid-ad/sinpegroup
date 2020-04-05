94598a606898eba65ba69a2a924ecb27e467e066
Omid Adib <akbaradibfar4@gmail.com>
2020-04-05 18:34:49 +0430

    improve some methods in utils.py

--- mainapp/utils.py
+++ mainapp/utils.py
@@ -1,17 +1,27 @@
 from .models import *
 
 
-def get_crew_members():
-    return Crew.objects.filter(active=True)[:6]
+def get_crew_members(amount):
+    if amount == 'all':
+        return Crew.objects.filter(active=True)
+    return Crew.objects.filter(active=True)[:amount]
 
 
-def get_portfolios():
-    return Portfolio.objects.filter(active=True)[:3]
+def get_portfolios(amount):
+    if amount == 'all':
+        return Portfolio.objects.filter(active=True)
+    return Portfolio.objects.filter(active=True)[:amount]
 
 
-def get_services():
-    return Service.objects.filter(active=True)[:6]
+def get_services(amount):
+    if amount == 'all':
+        return Service.objects.filter(active=True)
+    return Service.objects.filter(active=True)[:amount]
 
 
 def get_landingpage_desc():
     return LandingDescription.objects.first()
+
+
+def get_port_desc():
+    return PortfolioDescription.objects.first()
